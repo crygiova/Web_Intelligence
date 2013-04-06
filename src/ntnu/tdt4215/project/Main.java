bfd3799ed3221e2f91a0f352d997d1a9e20a9505
crygiova <crygiova@gmail.com>
2013-04-06 14:08:43 +0200

    FirstCommit
    I have create

---
+++ src/ntnu/tdt4215/project/Main.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package ntnu.tdt4215.project;
+
+/**
+ * @author christiangiovanelli
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
