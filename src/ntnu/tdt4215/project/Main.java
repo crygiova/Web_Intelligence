ca78defb0fa31a9ef858dfd94acfeda3ff1c9a37
gonch <gonchos@gmail.com>
2013-04-06 15:28:09 +0200

    reading OWL

--- src/ntnu/tdt4215/project/Main.java
+++ src/ntnu/tdt4215/project/Main.java
@@ -23,7 +23,8 @@ package ntnu.tdt4215.project;
 
 // Imports
 ///////////////
-import com.hp.hpl.jena.ontology.*;
+import com.hp.hpl.jena.ontology.OntModel;
+import com.hp.hpl.jena.ontology.OntModelSpec;
 import com.hp.hpl.jena.rdf.model.ModelFactory;
 
 
@@ -32,7 +33,7 @@ import com.hp.hpl.jena.rdf.model.ModelFactory;
  * Execution wrapper for class hierarchy example
  * </p>
  */
-public class Main {
+public class Main  {
     // Constants
     //////////////////////////////////
 
@@ -53,18 +54,35 @@ public class Main {
         OntModel m = ModelFactory.createOntologyModel( OntModelSpec.OWL_MEM, null );
 
         // we have a local copy of the wine ontology
-        m.getDocumentManager().addAltEntry( "http://www.w3.org/2001/sw/WebOnt/guide-src/wine",
-                                            "file:testing/reasoners/bugs/wine.owl" );
-        m.getDocumentManager().addAltEntry( "http://www.w3.org/2001/sw/WebOnt/guide-src/wine.owl",
-                                            "file:testing/reasoners/bugs/wine.owl" );
-        m.getDocumentManager().addAltEntry( "http://www.w3.org/2001/sw/WebOnt/guide-src/food",
-                                            "file:testing/reasoners/bugs/food.owl" );
-        m.getDocumentManager().addAltEntry( "http://www.w3.org/2001/sw/WebOnt/guide-src/food.owl",
-                                            "file:testing/reasoners/bugs/food.owl" );
-
-        m.read( "http://www.w3.org/2001/sw/WebOnt/guide-src/wine" );
-
+//        m.getDocumentManager().addAltEntry( "http://www.w3.org/2001/sw/WebOnt/guide-src/wine",
+//                                            "file:testing/reasoners/bugs/wine.owl" );
+//        m.getDocumentManager().addAltEntry( "http://www.w3.org/2001/sw/WebOnt/guide-src/wine.owl",
+//                                            "file:testing/reasoners/bugs/wine.owl" );
+//        m.getDocumentManager().addAltEntry( "http://www.w3.org/2001/sw/WebOnt/guide-src/food",
+//                                            "file:testing/reasoners/bugs/food.owl" );
+//        m.getDocumentManager().addAltEntry( "http://www.w3.org/2001/sw/WebOnt/guide-src/food.owl",
+//                                            "file:testing/reasoners/bugs/food.owl" );
+//
+        m.getDocumentManager().addAltEntry( "http://www.w3.org/2001/sw/WebOnt/guide-src/icd10",
+              "file:owl/icd10no.owl" );
+        m.read( "http://www.w3.org/2001/sw/WebOnt/guide-src/icd10" );
         new ClassHierarchy().showHierarchy( System.out, m );
+//    	// create an empty model
+//    	 Model model = ModelFactory.createDefaultModel();
+//
+//    	 // use the FileManager to find the input file
+//    	 String inputFileName="owl/icd10no_turtle.owl";
+//    	 InputStream in = FileManager.get().open( inputFileName );
+//    	if (in == null) {
+//    	    throw new IllegalArgumentException(
+//    	                                 "File: " + inputFileName + " not found");
+//    	}
+//
+//    	// read the RDF/XML file
+//    	model.read(in, null);
+//
+//    	// write it to standard out
+//    	model.write(System.out);
     }
 
 
