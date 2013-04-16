cb9ecdbe831fbdba8257a7b8cd736071e10a0196
crygiova <crygiova@gmail.com>
2013-04-16 20:54:15 +0200

    Good luck gonzalo

--- src/ntnu/tdt4215/project/IOFileTxt.java
+++ src/ntnu/tdt4215/project/IOFileTxt.java
@@ -28,14 +28,338 @@ public class IOFileTxt {
     private final static String PRTNFILE = "File not found";
     private final static String PRTNREAD = "File not read";
     private final static String PRTNWRITE = "Writing nt done";
-    private final static String PRTNEND = "Il file non stato chiuso correttamente";
+    private final static String FINISHED = "END";
 
     private final static String DOCU_FOLDER = "./docu/";
     private final static String T_FOLDER = DOCU_FOLDER + "NLH/T/";
     private final static String OPEN_DOC = "\n<doc>\n";
     private final static String CLOSE_DOC = "\n</doc>\n";
 
-    public static String parseHTM(String fnameInput, String fnameOut)
+    private static File inputF;
+
+    private final static String[] CLASSES_HANDLED = { "seksjon2", "seksjon3",
+	    "seksjon4", "seksjon8" };// , "seksjon8" };
+    private final static String[] TAGS_SKIPED = { "br", "input", "img", "tr",
+	    "hr", "ol" };
+
+    private final static String H1 = "h1";
+    private final static String H2 = "h2";
+    private final static String H3 = "h3";
+    private final static String H4 = "h4";
+    private final static String H5 = "h5";
+    private final static String[] TAG = { H2, H3, H4, H5 };
+
+    private final static String[] CLASS_NO_HANDLE = { "", "revidert" };
+    private final static String DIV_SUB8 = "sub8";
+    private final static String TAG_ARTICLE = "article";
+    private final static String END_ARTICLE = "/article";
+    private final static String CLASS_TEXT = "defa";
+    private final static String CLASS_LINK = "tonea";
+    private static final String PRTNEND = null;
+
+    // this function parse a html of the 3rd level
+    public static String parseHTML(String fnameInput, String fnameOut)
+	    throws IOException {
+	// opening the input file
+	inputF = new File(fnameInput);
+	Document mainDoc = Jsoup.parse(inputF, "UTF-8");
+	String mainTitle;
+	// I have the article
+	Element article = mainDoc.getElementsByTag(TAG_ARTICLE).get(0);
+	mainTitle = article.getElementsByTag(H2).get(0).text();
+	// TODO splitTitle(mainTitle);
+	// elements in the article
+	// children of the article seksjon2
+
+	Elements articleChildren = article.children();
+	extractSubSeksjon(articleChildren, 0, 0, "");
+	// for (int i = 0; i < articleChildren.size(); i++) {
+	// // IF IS A SEKSJON 2
+	// if (articleChildren.get(i).className()
+	// .compareToIgnoreCase(CLASSES_HANDLED[0]) == 0) {
+	// System.out.println(articleChildren.get(i).tagName() + " "
+	// + extractTitle(articleChildren.get(i), H2));
+	// // get the child of a seksjon2
+	// extractSubSeksjon(articleChildren.get(i).children(), 1, H3,
+	// "\t");
+	// }
+	//
+	// }
+
+	// // if the article has seksjon2
+	// if (article.getElementsByClass(CLASSES_HANDLED[0]).size() > 0) {
+	// Elements sek2 = article.getElementsByClass(CLASSES_HANDLED[0]);
+	// // for every section 2 -> look for sub8 and for seksjon
+	// // MAIN TITLE OF THE ARTICLE
+	//
+	// if (article.getElementsByClass(DIV_SUB8).size() > 0) {
+	//
+	// }
+	// }
+	return fnameInput;
+
+    }
+
+    private static String extractSubSeksjon(Elements seksjonChildren,
+	    int indexClass, int indexH, String useless) {
+
+	String content = "";
+	if (indexClass >= CLASSES_HANDLED.length || indexH >= TAG.length) {
+	    return "END";
+	}
+	for (int j = 0; j < seksjonChildren.size(); j++) {
+	    // if they are in the section 3
+
+	    if (seksjonChildren.get(j).className()
+		    .compareToIgnoreCase(CLASSES_HANDLED[indexClass]) == 0) {
+		// System.out.println(useless + seksjonChildren.get(j).tagName()
+		// + " "
+		// + extractTitle(seksjonChildren.get(j), TAG[indexH])
+		// + " " + seksjonChildren.get(j).className());
+
+		System.out.println(useless + " "
+			+ extractTitle(seksjonChildren.get(j), TAG[indexH])
+			+ " " + seksjonChildren.get(j).className());
+
+		extractContentSeksjon(seksjonChildren.get(j), indexClass);
+		extractSubSeksjon(seksjonChildren.get(j).children(),
+			indexClass + 1, indexH + 1, useless + "\t");
+
+	    }
+	}
+	return content;
+
+    }
+
+    private static String extractContentSeksjon(Element element, int indexClass) {
+	String content = "";
+	Elements children = element.children();
+	Element e;
+	switch (indexClass) {
+
+	// s 2
+	case 0:
+
+	    for (int i = 0; i < children.size(); i++) {
+		e = children.get(i);
+		if (isBreak2(e)) {
+		    if (iHaveToTakeIt2(e)) {
+			content += goDeeply2(e);
+		    }
+		} else {
+		    return content;
+		}
+	    }
+	    break;
+	// s 3
+	case 1:
+
+	    for (int i = 0; i < children.size(); i++) {
+		e = children.get(i);
+		if (isBreak3(e)) {
+		    if (iHaveToTakeIt3(e)) {
+			// System.out.println(i + " " + e.text());
+			content += goDeeply2(e);
+		    }
+		} else {
+		    return content;
+		}
+	    }
+
+	    break;
+	// s 4
+	case 2:
+	    for (int i = 0; i < children.size(); i++) {
+		e = children.get(i);
+		if (isBreak4(e)) {
+		    if (iHaveToTakeIt4(e)) {
+			// System.out.println(i + " " + e.text());
+			content += goDeeply2(e);
+		    }
+		} else {
+		    return content;
+		}
+	    }
+	    break;
+
+	case 3: // s 8
+	    for (int i = 0; i < children.size(); i++) {
+		e = children.get(i);
+		//
+		if (isBreak8(e)) {
+		    if (iHaveToTakeIt8(e)) {
+			content += goDeeply2(e);
+		    }
+		} else {
+		    return content;
+		}
+	    }
+	    break;
+	}
+	return content;
+    }
+
+    private static String goDeeply2(Element child) {
+	// Elements children = e.children();
+	String content = "";
+	// goo down in the document
+	if (iHaveToGo(child)) {
+	    for (int i = 0; i < child.children().size(); i++) {
+		goDeeply2(child.children().get(i));
+	    }
+	} else // is a son with a content
+	if (!child.text().isEmpty()
+		&& (child.text().compareTo(" ") != 0 && !inVector(TAGS_SKIPED,
+			child.tagName()))) {
+	    System.out.println(child.tagName() + " : " + child.text());
+	}
+	return content;
+    }
+
+    // goo down in the HTML to find the tag that contains text
+    private static String goDeeply(Element e) {
+	Elements children = e.children();
+	String content = "";
+	for (int i = 0; i < children.size(); i++) {
+	    Element child = children.get(i);
+	    if (iHaveToGo(child)) {
+		goDeeply(child);
+	    } else {
+		if (!child.text().isEmpty()
+			&& (child.text().compareTo(" ") != 0)
+			&& !inVector(TAGS_SKIPED, child.tagName()))
+		    ;// System.out.println(child.tagName() + " : " +
+		     // child.text());
+	    }
+	}
+	return content;
+    }
+
+    private static boolean iHaveToGo(Element child) {
+	return (child.tagName().compareTo("div") == 0);
+    }
+
+    private static String createXML(Element e) {
+	Elements children = e.children();
+	for (int i = 0; i < children.size(); i++) {
+	    Element child = children.get(i);
+	    System.out.println(child.tagName() + " text: " + child.text());
+	}
+	return null;
+    }
+
+    private static boolean iHaveToTakeIt8(Element e) {
+	return (e.className().compareTo("revidert") != 0
+		&& e.text().compareTo("") != 0
+		&& !inVector(TAGS_SKIPED, e.tagName()) && e.tagName()
+		.compareTo(H5) != 0);
+    }
+
+    private static boolean isBreak8(Element e) {
+	return (e.className().compareTo("seksjon3") != 0)
+		&& (e.className().compareTo("seksjon4") != 0);
+    }
+
+    private static boolean iHaveToTakeIt2(Element e) {
+	return (e.className().compareTo("revidert") != 0 && !e.text().isEmpty()
+		&& !inVector(TAG, e.tagName()) && !inVector(TAGS_SKIPED,
+		    e.tagName()));
+    }
+
+    private static boolean isBreak2(Element e) {
+	// TODO Auto-generated method stub
+	return (e.className().compareTo("seksjon3") != 0)
+		&& (e.className().compareTo("seksjon4") != 0);
+    }
+
+    private static boolean iHaveToTakeIt4(Element e) {
+	return (e.className().compareTo("revidert") != 0
+		&& e.text().compareTo("") != 0 && !inVector(TAG, e.tagName()) && !inVector(
+		    TAGS_SKIPED, e.tagName()));
+    }
+
+    private static boolean isBreak4(Element e) {
+	// TODO Auto-generated method stub
+	return (e.className().compareTo("seksjon8") != 0);
+    }
+
+    private static boolean iHaveToTakeIt3(Element e) {
+	return (e.className().compareTo("revidert") != 0
+		&& e.text().compareTo("") != 0 && !inVector(TAG, e.tagName()) && !inVector(
+		    TAGS_SKIPED, e.tagName()));
+    }
+
+    private static boolean isBreak3(Element e) {
+	// TODO Auto-generated method stub
+	return (e.className().compareTo("seksjon4") != 0);
+    }
+
+    public static boolean inVector(String[] vector, String name) {
+	for (String obj : vector) {
+	    if (name.compareToIgnoreCase(obj) == 0) {
+		return true;
+	    }
+	}
+	return false;
+    }
+
+    private static String extractSubSeksjon(Elements seksjon2Children,
+	    int indexClass, String H, String useless) {
+
+	if (indexClass >= CLASSES_HANDLED.length) {
+	    return FINISHED;
+	}
+	for (int j = 0; j < seksjon2Children.size(); j++) {
+	    // if they are in the section 3
+	    if (seksjon2Children.get(j).className()
+		    .compareToIgnoreCase(CLASSES_HANDLED[indexClass]) == 0) {
+		String title = extractTitle(seksjon2Children.get(j), H);
+		System.out.println(useless + seksjon2Children.get(j).tagName()
+			+ " " + title + " "
+			+ seksjon2Children.get(j).className());
+		extractSubSeksjon(seksjon2Children.get(j).children(),
+			indexClass, H4, useless + "\t");
+	    }
+	}
+	return null;
+
+    }
+
+    private static String extractTitle(Element element, String h32) {
+	// TODO Auto-generated method stub
+
+	if (element.getElementsByTag(h32).size() > 0) {
+	    return element.getElementsByTag(h32).get(0).text();
+
+	}
+	return "";
+    }
+
+    public static String[] splitTitle(String mainTitle) {
+
+	String[] title = new String[2];
+	for(int i =0;i<mainTitle.length();i++)
+	{
+	   
+	}
+	return title;
+
+    }
+
+    private static String extractContent(Element e) {
+	return null;
+
+    }
+
+    private static boolean elementContainClass(Element article, String string) {
+	// TODO Auto-generated method stub
+	if (article.getElementsByClass(string).size() > 0) {
+	    return true;
+	}
+	return false;
+    }
+
+    public static String mainParserHtml(String fnameInput, String fnameOut)
 	    throws IOException {
 
 	boolean first = true;
@@ -58,10 +382,10 @@ public class IOFileTxt {
 		doc = Jsoup.parse(input, "UTF-8");
 		Element bodyframe2nd = doc.getElementById("bodyframe");
 		// extract the title of 2nd level
-		Element titleH1 = bodyframe2nd.getElementsByTag("h1").get(0);
-		System.out.println(titleH1.text());
-		document2nd += createField("id", titleH1.text());
-		// TODO handle the relates links
+		// Element titleH1 = bodyframe2nd.getElementsByTag("h1").get(0);
+		// System.out.println(titleH1.text());
+		// document2nd += createField("id", titleH1.text());
+		// // TODO handle the relates links
 		// TODO extract also the others contents
 
 		// SUBCLASSES from the table
@@ -76,28 +400,29 @@ public class IOFileTxt {
 		    hrefOf3rdLevel = hrefOf3rdLevel.split("#")[0];
 		    if (hrefOf3rdLevel.compareTo(".htm") != 0) {
 			// Split the link T1.1.htm#i264
+			parseHTML(T_FOLDER + hrefOf3rdLevel, hrefOf3rdLevel);
 
-			String contentOf3rdLevel = link3rdLevel.text();
-			document2nd += createField("subClass",
-				contentOf3rdLevel);
-			// System.out.println(hrefOf3rdLevel + " \t "
-			// + contentOf3rdLevel);
-			String thirdLevelFile = T_FOLDER + hrefOf3rdLevel;
-
-			// third level file
-			String thirdLevelXML = extract3rdXML(thirdLevelFile);
-			// TODO write thirdlevelXML
-			input = new File(thirdLevelFile);
-			doc = Jsoup.parse(input, "UTF-8");
-
-			Element article = doc.getElementsByTag("article")
-				.get(0);
+			// String contentOf3rdLevel = link3rdLevel.text();
+			// document2nd += createField("subClass",
+			// contentOf3rdLevel);
+			// // System.out.println(hrefOf3rdLevel + " \t "
+			// // + contentOf3rdLevel);
+			// String thirdLevelFile = T_FOLDER + hrefOf3rdLevel;
+			//
+			// // third level file
+			// String thirdLevelXML = extract3rdXML(thirdLevelFile);
+			// // TODO write thirdlevelXML
+			// input = new File(thirdLevelFile);
+			// doc = Jsoup.parse(input, "UTF-8");
+			//
+			// Element article = doc.getElementsByTag("article")
+			// .get(0);
 			// System.out.println(article.attr("id") + " ");
 		    }
 		}
 		// CLOSE THE DOCU AND AFTER I SHOULD PRINT IN A FILE
 		document2nd += CLOSE_DOC;
-		System.out.println(document2nd);
+		// System.out.println(document2nd);
 
 	    } else {
 		return "END";
--- src/ntnu/tdt4215/project/Main.java
+++ src/ntnu/tdt4215/project/Main.java
@@ -13,44 +13,51 @@ import org.apache.solr.common.SolrDocumentList;
 public class Main {
     private final static String DOCU_FOLDER = "./docu/";
     private final static String T_FOLDER = DOCU_FOLDER + "NLH/T/";
+    private final static String EASY = "T6.6.htm";
+    private final static String MEDIUM = "T1.1.htm";
+    private final static String DIFFICULT = "T3.1.htm";
 
-    public static void main(String[] args) throws IOException, SolrServerException {
+    public static void main(String[] args) throws IOException,
+	    SolrServerException {
 	System.out.println("WORKING ..........");
-
-	final String str = "    <exclusion rdf:resource=\"&icd10no;B92\"/> ";
+	// System.out.println(IOFileTxt.parseHTML(T_FOLDER + EASY, "easy"));
+	// System.out.println(IOFileTxt.parseHTML(T_FOLDER + MEDIUM, "medium"));
+	System.out.println(IOFileTxt.parseHTML(T_FOLDER + DIFFICULT,
+		"difficult"));
+	String titolo [] =IOFileTxt.splitTitle("T3.1.4 Spesielle problemer");
+	
+	for (String t :titolo)	
+	{
+	    System.out.println("++"+t+"--");
+	}
+	// IOFileTxt.mainParserHtml(T_FOLDER + "innhold.htm", "easy");
 	// final Pattern pattern = Pattern.compile(">.*</");
 	// RIGHT TODO ->content final Pattern pattern =
 	// Pattern.compile(".*>(.*?)<.*");
 	// RIGHT TODO -> name final Pattern pattern =
 	// Pattern.compile("<(.*?)\\s");
 	// Parser.parserOWL("newIcd.owl","jose.xml");
-	System.out.println(IOFileTxt.parseHTM(T_FOLDER + "innhold.htm", ""));
-	
-	System.out.println("WORKING ..........");
-	String url = "http://localhost:8983/solr";
-	SolrServer server = new HttpSolrServer(url);
-	((HttpSolrServer) server).setParser(new XMLResponseParser());
-//	SolrInputDocument doc1 = new SolrInputDocument();
-//	doc1.addField( "id", "id1", 1.0f );
-//	doc1.addField( "name", "doc1", 1.0f );
-//	doc1.addField( "price", 10 );
-//	SolrInputDocument doc2 = new SolrInputDocument();
-//	doc2.addField( "id", "id2", 1.0f );
-//	doc2.addField( "name", "doc2", 1.0f );
-//	doc2.addField( "price", 20 );		
-//	Collection<SolrInputDocument> docs = new ArrayList<SolrInputDocument>();
-//	docs.add( doc1 );
-//	docs.add( doc2 );
-//	server.add(docs);
-//	server.commit();
-	SolrQuery query = new SolrQuery();
-	query.setQuery("Eva Andersen er en skoleelev som har hatt insulinkrevende diabetes mellitus i 3 år");
-	query.addField("id");
-	QueryResponse rsp = server.query( query );
-	SolrDocumentList docs = rsp.getResults();
-	for(int i=0;i<docs.size();i++){
-	    System.out.println(docs.get(i).toString());
-	}
+	/*
+	 * System.out.println(IOFileTxt.parseHTM(T_FOLDER + "innhold.htm", ""));
+	 * 
+	 * System.out.println("WORKING .........."); String url =
+	 * "http://localhost:8983/solr"; SolrServer server = new
+	 * HttpSolrServer(url); ((HttpSolrServer) server).setParser(new
+	 * XMLResponseParser()); // SolrInputDocument doc1 = new
+	 * SolrInputDocument(); // doc1.addField( "id", "id1", 1.0f ); //
+	 * doc1.addField( "name", "doc1", 1.0f ); // doc1.addField( "price", 10
+	 * ); // SolrInputDocument doc2 = new SolrInputDocument(); //
+	 * doc2.addField( "id", "id2", 1.0f ); // doc2.addField( "name", "doc2",
+	 * 1.0f ); // doc2.addField( "price", 20 ); //
+	 * Collection<SolrInputDocument> docs = new
+	 * ArrayList<SolrInputDocument>(); // docs.add( doc1 ); // docs.add(
+	 * doc2 ); // server.add(docs); // server.commit(); SolrQuery query =
+	 * new SolrQuery(); query.setQuery(
+	 * "Eva Andersen er en skoleelev som har hatt insulinkrevende diabetes mellitus i 3 år"
+	 * ); query.addField("id"); QueryResponse rsp = server.query( query );
+	 * SolrDocumentList docs = rsp.getResults(); for(int
+	 * i=0;i<docs.size();i++){ System.out.println(docs.get(i).toString()); }
+	 */
 	System.out.println("done");
     }
 }
