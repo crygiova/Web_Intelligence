317ea75b31cb8ebb09fb0363429b28884850dce9
crygiova <crygiova@gmail.com>
2013-04-19 11:24:39 +0200

    TAble commit

--- src/ntnu/tdt4215/project/Table.java
+++ src/ntnu/tdt4215/project/Table.java
@@ -1,22 +1,27 @@
 package ntnu.tdt4215.project;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 
 public class Table implements java.io.Serializable{
     private static final long serialVersionUID = 1L;
-    private HashMap<Coordinates,ArrayList<StringAndFloat>> table;
+    private ArrayList <Row> table;
 
     public Table(){
-	table = new HashMap<Coordinates,ArrayList<StringAndFloat>>();
+	table = new ArrayList <Row>();
     }
 
     public void setMatching (String note, int sentence, ArrayList<StringAndFloat> list) {
-	table.put(new Coordinates(note, sentence), list);
+	table.add(new Row(note, sentence, list));
     }
 
-    public ArrayList<StringAndFloat> getMatches(String note,int sentence) {
-	return table.get(new Coordinates(note,sentence));
+    public ArrayList<ArrayList<StringAndFloat>> getMatches(String id) {
+	ArrayList<ArrayList<StringAndFloat>> result = new ArrayList<ArrayList<StringAndFloat>>();
+	for(int i = 0;i<table.size();i++){
+	    if(table.get(i).getId().compareTo(id)==0) {
+		result.add(table.get(i).getMatches());
+	    }
+	}
+	return result;
     }
 
     public int size(){
@@ -24,22 +29,24 @@ public class Table implements java.io.Serializable{
     }
 }
 
-class Coordinates {
-
-    private String note;
+class Row {
+    private String id;
     private int sentence;
+    private ArrayList<StringAndFloat> matches;
 
-    public Coordinates(String x, int y) {
-	setNote(x);
-	setSentence(y);
+
+    public Row(String id, int sentence, ArrayList<StringAndFloat> table){
+	this.id = id;
+	this.sentence = sentence;
+	this.matches = table;
     }
 
-    public String getNote() {
-	return note;
+    public String getId() {
+	return id;
     }
 
-    public void setNote(String note) {
-	this.note = note;
+    public void setId(String id) {
+	this.id = id;
     }
 
     public int getSentence() {
@@ -50,17 +57,54 @@ class Coordinates {
 	this.sentence = sentence;
     }
 
-    @Override
-    public boolean equals(Object o) {
-	Coordinates c = (Coordinates) o;
-	return(this.note.compareTo(c.getNote())==0 && this.sentence==c.getSentence());
+    public ArrayList<StringAndFloat> getMatches() {
+	return matches;
     }
 
-    @Override
-    public int hashCode(){
-	StringBuilder builder = new StringBuilder();
-	builder.append(note);
-	builder.append(sentence);
-	return builder.toString().hashCode();
+    public void setMatches(ArrayList<StringAndFloat> table) {
+	this.matches = table;
     }
+
+
 }
+
+//class Coordinates {
+//
+//    private String note;
+//    private int sentence;
+//
+//    public Coordinates(String x, int y) {
+//	setNote(x);
+//	setSentence(y);
+//    }
+//
+//    public String getNote() {
+//	return note;
+//    }
+//
+//    public void setNote(String note) {
+//	this.note = note;
+//    }
+//
+//    public int getSentence() {
+//	return sentence;
+//    }
+//
+//    public void setSentence(int sentence) {
+//	this.sentence = sentence;
+//    }
+//
+//    @Override
+//    public boolean equals(Object o) {
+//	Coordinates c = (Coordinates) o;
+//	return(this.note.compareTo(c.getNote())==0 && this.sentence==c.getSentence());
+//    }
+//
+//    @Override
+//    public int hashCode(){
+//	StringBuilder builder = new StringBuilder();
+//	builder.append(note);
+//	builder.append(sentence);
+//	return builder.toString().hashCode();
+//    }
+//}
